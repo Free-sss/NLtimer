4f0d521c292ad81f479dcc1d03ead1b46a0e6ec2
Arsucar <630042479@qq.com>
2026-04-27 14:16:30 +0800

    chore(buildSrc): add nltimer.android.application convention plugin

---
+++ buildSrc/src/main/kotlin/nltimer.android.application.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    compileSdk = SdkVersions.compileSdk
+
+    defaultConfig {
+        minSdk = SdkVersions.minSdk
+        targetSdk = SdkVersions.targetSdk
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = false
+    }
+
+    lint {
+        warningsAsErrors = false
+        abortOnError = false
+        disable("GradleDependency")
+    }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
+}
