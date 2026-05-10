dfa865f2a8b558156f204c77cb169c67537eac85
Arsucar <630042479@qq.com>
2026-05-10 19:53:15 +0800

    build: 添加 kotlinx.serialization 依赖

--- feature/settings/build.gradle.kts
+++ feature/settings/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("nltimer.android.library")
     id("nltimer.android.hilt")
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -18,6 +19,7 @@ dependencies {
     implementation(libs.lifecycle.viewmodel.compose)
 
     implementation(libs.materialkolor)
+    implementation(libs.kotlinx.serialization.json)
 
     implementation(platform(libs.compose.bom))
     implementation(libs.compose.material3)
